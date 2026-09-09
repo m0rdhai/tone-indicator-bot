c531a43372b5e58157b13b0f5af2e3903de628fc
Johanna Rossoll <if25b502@technikum-wien.at>
2026-09-09 18:57:54 +0200

    Add additional tone indicators with descriptions and codes

--- tones.js
+++ tones.js
@@ -6,8 +6,38 @@ module.exports = {
         description: 'meant to be humorous or sarcastic',
     },
     s: {
+        code: '0;31',
+        label: 'sarcastic',
+        description: 'using irony to mock or convey contempt',
+    },
+    gen: {
+        code: '0;32',
+        label: 'genuine',
+        description: 'sincere or authentic',
+    },
+    srs: {
         code: '0;34',
         label: 'serious',
         description: 'meant to be taken seriously',
+    },
+    nsrs: {
+        code: '0;35',
+        label: 'not serious',
+        description: 'not meant to be taken seriously',
+    },
+    pos: {
+        code: '0;37',
+        label: 'positive',
+        description: 'expressing approval or optimism',
+    },
+    neg: {
+        code: '0;30',
+        label: 'negative',
+        description: 'expressing disapproval or pessimism',
+    },
+    ij: {
+        code: '0;36',
+        label: 'inside joke',
+        description: 'a joke that is understood only by a specific group of people',
     },
  };
